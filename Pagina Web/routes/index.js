b59331425ff1f9a0dfeaddb12463b112e4bb7b7a
beds10 <73314223+beds10@users.noreply.github.com>
2021-01-25 17:59:40 -0600

    VOTOS

--- Pagina Web/routes/index.js
+++ Pagina Web/routes/index.js
@@ -4,7 +4,7 @@ var bd = require("../conexion/conexion");
 /* GET home page.(index.ejs) */
 router.get('/', function(req, res, next) {
   //hacemos el select con la variable importada de conexión 
-    bd.query("SELECT SUBSTRING('descripcion', 1, 150) as descripcion, titulo, imagen FROM tbl_videojuegos", function(err,listjuegos){
+    bd.query("SELECT SUBSTRING(descripcion, 1, 60) as descripcion, titulo, imagen, votos FROM tbl_videojuegos", function(err,listjuegos){
         console.log(listjuegos);  
         res.render('index', { title: 'VideoGamesCity', Videojuegos:listjuegos  });
              
