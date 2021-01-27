279f2f9b1c1372c63c04d9cdc910cddad05217fc
beds10 <73314223+beds10@users.noreply.github.com>
2021-01-27 17:28:03 -0600

    UPDATE

--- Pagina Web/controllers/usuarios.js
+++ Pagina Web/controllers/usuarios.js
@@ -8,42 +8,55 @@ var bd = require("../conexion/conexion");
 exports.login = async (req, res) => {
     
     try {
-        const { correo, contrasena}= req.body;
+        const { correo, contrasena } = req.body;
   
         if( !correo || !contrasena ){
           return res.status(400).render('login', {
-            message : 'Por favor proporciona una contraseña o correo'
+            message : 'Por favor proporciona una contraseña o correo.'
           })
         }
   
-        bd.query('SELECT id_usuario, SUBSTRING(nombre, 1, 12) as nombre, apellido, contrasena, correo, id_tipouser FROM usuario WHERE correo = ?', [correo], async (error,results) =>{
-          console.log(results);
-          if( !results || !(await bcrypt.compare(contrasena, results[0].contrasena))) {
+        bd.query('SELECT * FROM usuario WHERE correo = ?', [correo], async (error, results) => {
+          if(error){
+            console.log(error);
+        }
+
+        if(results.length > 0) {
+          
+          if( !results[0].correo || !(await bcrypt.compare(contrasena, results[0].contrasena)) ) {
             res.status(401).render('login', {
-              message: 'Correo o Contraseña son incorrectas'
-            });
+              message: 'Correo o contraseña invalido.'
+            })
           } else {
+
+            
             const id = results[0].id_usuario;
-  
-            const token =jwt.sign({ id }, process.env.JWT_SECRET, {
+    
+            const token = jwt.sign({ id }, process.env.JWT_SECRET, {
               expiresIn: process.env.JWT_EXPIRES_IN
             });
-  
-            console.log("the token is:" + token);
-  
+    
+            console.log("The token is: " + token);
+    
             const cookieOptions = {
               expires: new Date(
                 Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000
               ),
-              httpOnly: true 
+              httpOnly: true
             }
-            res.cookie('jwt', token, cookieOptions);
-  
+    
+            res.cookie('jwt', token, cookieOptions );
             res.status(200).redirect("/");
-  
           }
-  
+        }else {
+            return res.render('login', {
+                message: 'Correo o contraseña invalido.'
+            });
+        }
+
+    
         })
+    
         
         
   
